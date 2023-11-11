720f4bbda9f31edd9c914f06a6c3bf96ad21126e
arielllyuan <zhiyangy@andrew.cmu.edu>
2023-11-11 15:00:35 -0800

    added webpage js and css

---
+++ functions.js
@@ -0,0 +1,11 @@
+function renderPage(page_id)
+{
+    if (page_id=="choice")
+    {
+        window.location.href="file:///Users/arielzhiyangyuan/Downloads/GWC%202023%20Github%20project/ArielGWC2023/choice.html";
+    }
+    else if (page_id=="game")
+    {
+        window.location.href="file:///Users/arielzhiyangyuan/Downloads/GWC%202023%20Github%20project/ArielGWC2023/game.html";
+    }
+}
